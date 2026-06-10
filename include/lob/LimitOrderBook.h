c0fee75512040e2b57306a4d86fa7c8835812c0b
Hamzah Ibrahim <hamzahfouad2006@gmail.com>
2026-06-10 18:40:16 +0100

    Implemented get_price_level and submit and defined others

--- include/lob/LimitOrderBook.h
+++ include/lob/LimitOrderBook.h
@@ -5,14 +5,28 @@
 #include <map>
 #include <functional>
 
-constexpr int64_t LADDER_DEPTH = 100;
+constexpr int64_t LADDER_DEPTH = 256; //must be power of 2
+constexpr int64_t MASK_MODULO = LADDER_DEPTH - 1;
 
 class LimitOrderBook {
     std::array<PriceLevel, LADDER_DEPTH> bid_ladder;
     std::array<PriceLevel, LADDER_DEPTH> ask_ladder;
 
-    std::map<int64_t, PriceLevel> bid_overflow;
+    int64_t bid_ladder_lower;
+    int64_t bid_ladder_higher;
+    int64_t ask_ladder_lower;
+    int64_t ask_ladder_higher;
+
+    int64_t best_bid = 0;
+    int64_t best_ask = INFINITY;
+
+    std::array<int64_t, LADDER_DEPTH/64> bid_bitmask;
+    std::array<int64_t, LADDER_DEPTH/64> ask_bitmask;
+
+    std::map<int64_t, PriceLevel, std::greater<int64_t>> bid_overflow;
     std::map<int64_t, PriceLevel> ask_overflow;
+
+    PriceLevel& get_price_level(Side side);
 
     public:
         OrderID submit(Side side, OrderType type, int64_t price, int64_t quantity);
--- src/lob/LimitOrderBook.cpp
+++ src/lob/LimitOrderBook.cpp
@@ -1,1 +1,80 @@
 #include "lob/LimitOrderBook.h"
+
+PriceLevel& LimitOrderBook::get_price_level(Side side) {
+    if (side == BUY) {
+        if (best_ask >= ask_ladder_lower && best_ask <= ask_ladder_higher) {
+            return ask_ladder[best_ask % LADDER_DEPTH];
+        }
+        return ask_overflow.begin()->second;
+    } else {
+        if (best_bid >= bid_ladder_lower && best_bid <= bid_ladder_higher) {
+            return bid_ladder[best_bid % LADDER_DEPTH];
+        }
+        return bid_overflow.begin()->second;
+    }
+}
+
+bool valid_price(Side side, int64_t book_best_price, int64_t order_price) {
+    if (side == BUY) {
+        if (order_price >= book_best_price) {return true;}
+        return false;
+    } else {
+        if (order_price <= book_best_price) {return true;}
+        return false;
+    }
+}
+
+void remove_price_level(Side side, int64_t price) {
+
+}
+
+void add_price_level(Side side, int64_t price, int64_t quantity);
+
+
+OrderID LimitOrderBook::submit(Side side, OrderType type, int64_t price, int64_t quantity) {
+    if (type == FOK) {
+        // get remaining liquidity above limit
+        // return if not enough liquidity
+        int64_t liquidity = available_liquidity(side, price);
+        if (liquidity < quantity) {return -1;}
+    }
+
+    int64_t remaining_quantity = quantity;
+    while (remaining_quantity > 0) {
+        //get best price and update best price quantity and remaining quantity
+        // exit early if book empty or no quantity better than price
+        if (available_liquidity(side, price) == 0) {break;}
+
+        PriceLevel& next_price_level = get_price_level(side);
+        if (!valid_price(side, next_price_level.price, price)) {break;}
+
+        while (remaining_quantity && next_price_level.quantity) {
+            Order& next_book_order = next_price_level.orders.front();
+
+            if (remaining_quantity >= next_book_order.quantity) {
+                remaining_quantity -= next_book_order.quantity;
+                next_price_level.quantity -= next_book_order.quantity;
+                next_price_level.orders.pop_front(); //order complete
+
+            } else {
+                next_price_level.quantity -= remaining_quantity;
+                next_book_order.quantity -= remaining_quantity;
+                remaining_quantity = 0;
+            }
+        }
+
+        if (next_price_level.quantity == 0) {
+            remove_price_level(side, price);
+        }
+    }
+
+    if (remaining_quantity && type == LIMIT) {
+        //store the remaining quantity
+        add_price_level(side, price, remaining_quantity);
+    }
+}
+
+
+int64_t available_liquidity(Side side, int64_t worst_price) {
+
+}
